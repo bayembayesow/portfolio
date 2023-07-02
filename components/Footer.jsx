0758455fd316f314f501290ce952c42a773f5d2a
bayembayesow <128776282+bayembayesow@users.noreply.github.com>
2023-07-01 21:56:01 -0400

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => {
+    return <div className={styles.footer}>
+        <p>&copy; Mbaye Sow - Développeur web Frontend</p>
+<footer>
+    {/* Contenu du pied de page */}
+<a href="https://www.linkedin.com/feed/?trk=404_page">LinkedIn</a>
+</footer>
+
+    </div>
+   
+};
+
+export default Footer;
