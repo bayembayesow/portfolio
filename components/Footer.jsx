38d2d7ea7192b311e59a68562428d21668dc6691
bayembayesow <128776282+bayembayesow@users.noreply.github.com>
2023-07-04 12:44:00 -0400

    Ajout du lien lnkedln et github

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,6 +6,12 @@ const Footer = () => {
 <footer>
     {/* Contenu du pied de page */}
 <a href="https://www.linkedin.com/feed/?trk=404_page">LinkedIn</a>
+
+</footer>
+
+<footer>
+    <a href="https://github.com/bayembayesow/portfolio">github</a>
+
 </footer>
 
     </div>
